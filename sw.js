f35e22b024d94f91a6bf1714cbd54fde0f417c9b
suturaseries-arch <sutura.series@gmail.com>
2026-08-24 15:36:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
 
+  event.waitUntil(
+    self.registration.showNotification(
+      data.title || "A Última Lâmina",
+      {
+        body: data.body || "Tem novidade no site!",
+        icon: data.icon || "/assets/icon.png",
+        badge: data.icon || "/assets/icon.png",
+        data: {
+          url: data.url || "/"
+        }
+      }
+    )
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
